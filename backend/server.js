805db6a7063a98c9d336ca4a462f5361b4526209
pwc88430 <pwc88430@uga.edu>
2026-01-20 20:40:36 -0700

    added url for production backend server

--- backend/server.js
+++ backend/server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "https://kickoffscheduler.app",
         methods: ["GET", "POST"],
     },
 });
